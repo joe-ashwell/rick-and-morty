8fb99d87e9682a986c2047613c90f7372121f0e8
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-10-21 08:42:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const endpoint = 'https://rickandmortyapi.com/api/character/'
 
-const words = [];
+const characters = [];
 
-// fetch returns a promise, which is..?
+let characterCount;
+
+let characterEndpoint;
+
+const getCharacterTotal = () => {
+
+  fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => characterCount = data.info.count);
+
+}
+
+const getCharacterInfo = () => {
+
+  // get url
+  // push info into characters array
+
+}
+
+// Fetch is a browser API. Fetch returns a promise, which doesn't actually deliver anything right away, so you need to tell it what to do next once it has the data?
 fetch(endpoint)
+  // need to turn the raw data into JSON. This new blob.json returns yet another promise, so we need to use 'then' again.
   .then(blob => blob.json())
-  .then(data => console.log(data));
+  // Then we can do something with the data returned.
+  // In this case we take the JSON and put it into the characters array. To avoid nesting by just doing characters.push(data), we use 'spread' which is ES6 syntax and goes through each item, and puts it into the array. Kinda like forEach??
+  .then(data => characterCount = data.info.count);
+  // .then(data => characters.push(...data))
+
+  for ( let c = 1; c < characterCount; c++ ) {
+    // characterEndpoint = `${endpoint} + ${c}`;
+    console.log('cunt');
+  }
