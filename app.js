7a934617f1a7c81516b9f03673f3ba417b5edd64
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-10-24 10:39:09 +0100

    Add the basic functionality to return results based on users search

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const endpoint = 'https://rickandmortyapi.com/api/character/'
 
 const characters = [];
 
+const searchInput = document.querySelector('input.search');
+const suggestions = document.querySelector('ul.suggestions');
+
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => {
@@ -15,5 +18,29 @@ fetch(endpoint)
         }
     });
 
+console.log(characters);
 
-    console.log(characters);
+// function to find matches between user search and characters array. Passing in the 'wordToMatch' which is what the user searches, and the character array to the function, so it knows what to search against.
+function findMatches(wordToMatch, characters) {
+  
+  // Return the filtered characters array
+  return characters.filter(character => {
+
+    // passes the 'wordToMatch' to the regular expression
+    // Flags; g = global, i = insensitive
+    const regex = new RegExp(wordToMatch, 'gi');
+    // returns a match against the regex and characters name or species
+    return character.name.match(regex) || character.species.match(regex);
+
+  });
+
+}
+
+function displayMatches() {
+
+  const matchValues = findMatches(this.value, characters);
+  console.log(matchValues);
+
+}
+
+searchInput.addEventListener('keyup', displayMatches);
